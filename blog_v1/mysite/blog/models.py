84bf93639005db9684daaa2ce1395f4210d93ce9
Mack <hypermail@yandex.ru>
2024-02-17 12:05:37 +0300

    canonic urls

--- blog_v1/mysite/blog/models.py
+++ blog_v1/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -18,7 +19,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -34,6 +35,9 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=["-publish"]),
         ]
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     def __str__(self):
         return self.title
--- blog_v1/mysite/blog/views.py
+++ blog_v1/mysite/blog/views.py
@@ -9,7 +9,9 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     """детали поста"""
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(
+        Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day
+    )
     return render(request, "blog/post/detail.html", {"post": post})
