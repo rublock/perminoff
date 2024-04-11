d5289833469e055bc2d0babef9452484c0bc812c
Mack <hypermail@yandex.ru>
2024-04-11 16:58:39 +0300

    ListView, DetailView, slug

--- blog_v2/apps/blog/views.py
+++ blog_v2/apps/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -10,4 +10,15 @@ class PostListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
         return context
