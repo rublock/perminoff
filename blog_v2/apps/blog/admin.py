7ec92de2ebe08c9dde1ba9fccaa0119b6bfba390
Mack <hypermail@yandex.ru>
2024-04-19 14:05:43 +0300

    comments tree

--- blog_v2/apps/blog/admin.py
+++ blog_v2/apps/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
-from .models import Category, Post
+from .models import Category, Post, Comment
 
 
 @admin.register(Category)
@@ -17,3 +17,11 @@ class PostAdmin(admin.ModelAdmin):
     Админ-панель модели записей
     """
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class CommentAdminPage(DjangoMpttAdmin):
+    """
+    Админ-панель модели комментариев
+    """
+    pass
--- blog_v2/apps/blog/urls.py
+++ blog_v2/apps/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostFromCategory, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostFromCategory, PostCreateView, PostUpdateView, CommentCreateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('post/create/', PostCreateView.as_view(), name='post_create'),
     path('post/<str:slug>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/comments/create/', CommentCreateView.as_view(), name='comment_create_view'),
     path('category/<slug:slug>/', PostFromCategory.as_view(), name="post_by_category"),
 ]
